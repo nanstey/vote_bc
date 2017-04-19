8855d3799860097b1d44942995acaec76768c88f
Couragyn Chretien <couragyn@gmail.com>
2017-04-19 21:52:47 +0000

    generated views and controllers for election show, district index & show

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
-  resources :districts
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root to: 'districts#index'
+
+  get '/election' => 'election#show'
+  get '/district' => 'district#index'
+  get '/district' => 'district#show'
+
 end
