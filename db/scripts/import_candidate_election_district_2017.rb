649ca99866510d3375234bcdc3dcaa178f1dbe0c
Noel Anstey <anstey.ng@gmail.com>
2017-04-20 17:49:00 -0700

    restructered seeding into multiple scripts. Seeded election results for 2013 and 2009

--- db/scripts/import_candidate_election_district_2017.rb
+++ db/scripts/import_candidate_election_district_2017.rb
@@ -1,39 +1,23 @@
 
-def seed_districts(csv)
-  District.destroy_all
-  puts "Seeding districts"
-  header = csv.row(1)
-  e = header.index('ED Code') + 1
-  d = header.index('Electoral District') + 1
-  (2..csv.last_row).each do |i|
-    pp District.find_or_create_by(name: csv.cell(i,d), abbr: csv.cell(i,e))
-  end
-end
-
-def seed_candidates(csv, year)
-  Candidate.destroy_all
-  CandidateElectionDistrict.destroy_all
-  puts "Seeding candidates & candidate_election_districts"
-  header = csv.row(1)
-  a = header.index('Affiliation') + 1
-  c = header.index('Candidate Ballot Name') + 1
-  d = header.index('Electoral District') + 1
-  pp election = Election.find_by(year: year)
-
-  (2..csv.last_row).each do |i|
-    pp party_name = csv.cell(i,a)
-    pp party = Party.find_by!(name: party_name)
-    pp district = District.find_by(name: csv.cell(i,d))
-    pp candidate = Candidate.find_or_create_by(name: csv.cell(i,c), party_id: party.id)
-    pp CandidateElectionDistrict.find_or_create_by(candidate_id: candidate.id, election_id: election.id, district_id: district.id)
-  end
-end
-
 puts "Reading candidates 2017 csv ..."
 
-candidates_2017 = Roo::CSV.new(Rails.root.join('db', 'excel', 'candidates_2017.csv'))
+csv = Roo::CSV.new(Rails.root.join('db', 'excel', 'candidates_2017.csv'))
 
-seed_districts(candidates_2017)
-seed_candidates(candidates_2017, 2017)
+header = csv.row(1)
+a = header.index('Affiliation') + 1
+c = header.index('Candidate Ballot Name') + 1
+d = header.index('Electoral District') + 1
+e = header.index('ED Code') + 1
+year = 2017
+election = Election.find_by(year: year)
 
-puts "DONE SEEDING"
+puts "Seeding districts & candidates for 2017 ..."
+
+(2..csv.last_row).each do |i|
+  party_name = csv.cell(i,a)
+  party = Party.find_by!(name: party_name)
+  district = District.find_or_create_by(name: csv.cell(i,d), abbr: csv.cell(i,e))
+  candidate = Candidate.find_or_create_by(name: csv.cell(i,c), party_id: party.id)
+  ElectionDistrict.find_or_create_by(election_id: election.id, district_id: district.id)
+  CandidateElectionDistrict.find_or_create_by(candidate_id: candidate.id, election_id: election.id, district_id: district.id)
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,53 +1,22 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-
-puts "Seeding ..."
 
 Party.destroy_all
+Election.destroy_all
+District.destroy_all
+Candidate.destroy_all
+ElectionLeader.destroy_all
+ElectionDistrict.destroy_all
+CandidateElectionDistrict.destroy_all
 
-puts "Seeding parties ..."
+puts "=============== START SEEDING ==============="
 
-Party.create!([
-  {name: 'BC Action Party', abbr: 'BCAP'},
-  {name: 'BC Cascadia Party', abbr: 'BCC'},
-  {name: 'BC Conservative Party', abbr: 'CP'},
-  {name: 'BC Excalibur Party', abbr: 'BCEX'},
-  {name: 'BC First', abbr: 'FP'},
-  {name: 'BC Green Party', abbr: 'GP'},
-  {name: 'BC Liberal Party', abbr: 'LIB'},
-  {name: 'BC Marijuana Party', abbr: 'BCM'},
-  {name: 'BC NDP', abbr: 'NDP'},
-  {name: 'BC Party', abbr: 'BCP'},
-  {name: 'BC Refed', abbr: 'REFD'},
-  {name: 'BC Social Credit Party', abbr: 'SC'},
-  {name: 'BC Vision Party', abbr: 'BCV'},
-  {name: 'Christian Heritage Party of B.C.', abbr: 'CHP' },
-  {name: 'Communist Party of BC', abbr: 'COMM'},
-  {name: 'Helping Hand Party', abbr: 'HH'},
-  {name: 'Independent', abbr: 'IND'},
-  {name: 'Libertarian', abbr: 'LBN'},
-  {name: 'National Alliance Party', abbr: 'NAP'},
-  {name: 'No Affiliation', abbr: ''},
-  {name: 'Other', abbr: 'OTH'},
-  {name: 'Peoples Front', abbr: 'PF'},
-  {name: 'Platinum Party', abbr: 'PLAT'},
-  {name: 'Republican Party', abbr: 'RP'},
-  {name: 'The Advocational Party', abbr: 'AID'},
-  {name: 'The Sex Party', abbr: 'SEX'},
-  {name: 'The Vancouver Island Party', abbr: 'VIP'},
-  {name: 'Unparty: The Consensus-Building Party', abbr: 'UCBP'},
-  {name: 'Western Canada Concept Party', abbr: 'WCC'},
-  {name: 'Work Less Party', abbr: 'WLP'},
-  {name: 'Your Political Party of BC (YPP)', abbr: 'YPBC'}
-])
+require Rails.root.join('db', 'scripts', 'seed_parties')
 
-puts "Seeding Election 2017"
-
-Election.find_or_create_by! year: 2017
+require Rails.root.join('db', 'scripts', 'seed_elections')
 
 require Rails.root.join('db', 'scripts', 'import_candidate_election_district_2017')
+
+require Rails.root.join('db', 'scripts', 'import_election_results_2013_2009')
+
+puts "=============== DONE SEEDING ==============="
