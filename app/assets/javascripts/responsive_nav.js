29c996371099cf45bd8a368003362c7e31bde762
Dace Hunter <dacehunter@gmail.com>
2017-04-21 01:34:08 +0000

    changed on document ready to on turbolinks:change in responsive_nav.js

--- app/assets/javascripts/responsive_nav.js
+++ app/assets/javascripts/responsive_nav.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+$(document).on("turbolinks:load", function() {
 // Toggles elections dropdown menu
   $('.dropdown-content').hide();
   $('.dropdown').on('click', function() {
