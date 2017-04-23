c9d0e468ff4738ecee05ed1469fcddfbe91afee4
Couragyn Chretien <couragyn@gmail.com>
2017-04-23 20:10:00 +0000

    added comments

--- app/controllers/district_controller.rb
+++ app/controllers/district_controller.rb
@@ -8,7 +8,6 @@ class DistrictController < ApplicationController
   end
 
   def search
-    puts "tried"
     geolocation = District.get_geolocation(params[:address])
     if district = District.get_district(geolocation)
       redirect_to "/district/#{district.slug}"
