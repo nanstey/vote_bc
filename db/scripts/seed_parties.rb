9059160bc8718430dcaef879b90e0038a8ca7cd5
Dace Hunter <dacehunter@gmail.com>
2017-04-28 11:51:36 -0700

    Update seed_parties.rb
    Changes no affiliation party color to #686868

--- db/scripts/seed_parties.rb
+++ db/scripts/seed_parties.rb
@@ -20,7 +20,7 @@ Party.create!([
   {name: 'Independent', abbr: 'IND', color: '#686868'},
   {name: 'Libertarian', abbr: 'LBN', color: '#f6c259'},
   {name: 'National Alliance Party', abbr: 'NAP', color: '#686868'},
-  {name: 'No Affiliation', abbr: 'N/A', color: '#DCDCDC'},
+  {name: 'No Affiliation', abbr: 'N/A', color: '#686868'},
   {name: 'Other', abbr: 'OTH', color: '#686868'},
   {name: 'Peoples Front', abbr: 'PF', color: '#68686'},
   {name: 'Platinum Party', abbr: 'PLAT', color: '#686868'},
