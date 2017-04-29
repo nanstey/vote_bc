e2d190d4a314d3d9987061fb179c0f51e3cb9ee3
Couragyn Chretien <couragyn@gmail.com>
2017-04-29 22:04:29 +0000

    assets compiled for Heroku

--- public/assets/application-cc952b18d28f62323f9a7230bdbe0e6e7aedf1b5163b1f5d3846332b1be3bb39.js
+++ public/assets/application-cc952b18d28f62323f9a7230bdbe0e6e7aedf1b5163b1f5d3846332b1be3bb39.js
@@ -12241,9 +12241,13 @@ $(document).on("turbolinks:load", function() {
 
 
 }).call(this);
-var currentYear = 2017 - 4;
-
 $(document).on("turbolinks:load", function() {
+  if (parseInt(window.location.pathname.slice(-4))) {
+    var currentYear = window.location.pathname.slice(-4);
+  }
+  else {
+    var currentYear = 2013;
+  }
 
   $(`.show-${currentYear}`).addClass('show');
 
@@ -12260,6 +12264,59 @@ $(document).on("turbolinks:load", function() {
 }).call(this);
 (function() {
 
+
+}).call(this);
+(function() {
+  this.GoogleAnalytics = (function() {
+    function GoogleAnalytics() {}
+
+    GoogleAnalytics.load = function() {
+      var firstScript, ga;
+      window._gaq = [];
+      window._gaq.push(["_setAccount", GoogleAnalytics.analyticsId()]);
+      ga = document.createElement("script");
+      ga.type = "text/javascript";
+      ga.async = true;
+      ga.src = ("https:" === document.location.protocol ? "https://ssl" : "http://www") + ".google-analytics.com/ga.js";
+      firstScript = document.getElementsByTagName("script")[0];
+      firstScript.parentNode.insertBefore(ga, firstScript);
+      if (typeof Turbolinks !== 'undefined' && Turbolinks.supported) {
+        return document.addEventListener("page:change", (function() {
+          return GoogleAnalytics.trackPageview();
+        }), true);
+      } else {
+        return GoogleAnalytics.trackPageview();
+      }
+    };
+
+    GoogleAnalytics.trackPageview = function(url) {
+      if (!GoogleAnalytics.isLocalRequest()) {
+        if (url) {
+          window._gaq.push(["_trackPageview", url]);
+        } else {
+          window._gaq.push(["_trackPageview"]);
+        }
+        return window._gaq.push(["_trackPageLoadTime"]);
+      }
+    };
+
+    GoogleAnalytics.isLocalRequest = function() {
+      return GoogleAnalytics.documentDomainIncludes("local");
+    };
+
+    GoogleAnalytics.documentDomainIncludes = function(str) {
+      return document.domain.indexOf(str) !== -1;
+    };
+
+    GoogleAnalytics.analyticsId = function() {
+      return 'UA-98305669-1';
+    };
+
+    return GoogleAnalytics;
+
+  })();
+
+  GoogleAnalytics.load();
 
 }).call(this);
 $(document).on("turbolinks:load", function() {
