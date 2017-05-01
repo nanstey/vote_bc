ce0ad37240a53f8c4c7ecd647de710d06e03f149
Dace Hunter <dacehunter@gmail.com>
2017-05-01 22:16:54 +0000

    moves party leaders to table. adds link to distric index

--- public/assets/application-0cebe7b75240b26fa63e018f9ba6102514431d44be07c404a940ca41e30e9c13.js
+++ public/assets/application-0cebe7b75240b26fa63e018f9ba6102514431d44be07c404a940ca41e30e9c13.js
@@ -12334,6 +12334,20 @@ $(document).on("turbolinks:load", function() {
   });
 
 }).call(this);
+/*document.addEventListener('turbolinks:before-visit', function(e) {
+  if (e.data.url.split(window.location.href)[1].length > 0) {
+    e.preventDefault();
+    window.location.href = e.data.url;
+  }
+});*/
+
+
+$(document).on('turbolinks:click', function (event) {
+  if (event.target.getAttribute('href').charAt(0) === '#') {
+    return event.preventDefault()
+  }
+})
+;
 $(document).on("turbolinks:load", function() {
 
   $(".translucent").delay(800).animate({ opacity: 1 }, 1300);
