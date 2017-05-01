4ee485e114c4ef06c8bbd730f887e719f1f51dd2
Couragyn Chretien <couragyn@gmail.com>
2017-05-01 00:37:43 +0000

    ordered by name

--- app/controllers/election_controller.rb
+++ app/controllers/election_controller.rb
@@ -9,7 +9,7 @@ class ElectionController < ApplicationController
     unless @election.premier_id.nil?
       @premier = Candidate.find(@election.premier_id)
     end
-    @election_districts = ElectionDistrict.includes(:district, winner: [:party]).where(:election_id => @election.id)
+    @election_districts = ElectionDistrict.includes(:district, winner: [:party]).where(:election_id => @election.id).order('districts.name')
     @leaders = @election.leaders
   end
 
