dda073cca5cd74ba7e6b3173b12d59ece34a530d
Dace Hunter <dacehunter@gmail.com>
2017-05-03 21:37:26 +0000

    removes accidentally commited flash logic

--- public/assets/application-93ccebafde5ff4f2274a3bbf33a467266cb8d4f0035c1d512742eeb2b9eca79d.js
+++ public/assets/application-93ccebafde5ff4f2274a3bbf33a467266cb8d4f0035c1d512742eeb2b9eca79d.js
@@ -12332,9 +12332,6 @@ $(document).on("turbolinks:load", function() {
 }).call(this);
   $(document).on("turbolinks:load", function() {
 
-  $('.election-donut').fadeTo(500, 1);
-
-
   $(window).on('resize', function() {
     try{
       if (!window.recentResize) {
