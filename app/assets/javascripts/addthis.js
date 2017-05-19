a6efc42d71e6b77536bee69853f9a8227997d647
Dace Hunter <dacehunter@gmail.com>
2017-05-19 01:12:20 +0000

    adds script to clear addthis values on turbolinks load

--- app/assets/javascripts/addthis.js
+++ app/assets/javascripts/addthis.js
@@ -1,20 +1,31 @@
-// turbolinks addthis
-var initAdthis;
-
-initAdthis = function(){
-    // Remove all global properties set by addthis, otherwise it won't reinitialize
-    for (var i in window) {
+var addthis;
+addthis = function() {
+    var script = 'http://s7.addthis.com/js/300/addthis_widget.js#pubid=ra-590931e84018f1e1#async=1';
+      // Remove all global properties set by addthis, otherwise it won't reinitialize
+      for (var i in window) {
         if (/^addthis/.test(i) || /^_at/.test(i)) {
-            delete window[i];
+          delete window[i];
         }
-    }
-    window.addthis_share = null;
+      }
+      window.addthis_share = null;
 
-    // Finally, load addthis
-    $.getScript("//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-590931e84018f1e1");
+      // Remove all global properties set by addthis, otherwise it won't reinitialize
+      if(window.addthis) {
+           window.addthis = null;
+            window._adr = null;
+            window._atc = null;
+            window._atd = null;
+            window._ate = null;
+            window._atr = null;
+            window._atw = null;
+      }
+
+      // Finally, load addthis
+      $.getScript(script, function(){
+        addthis.toolbox('.addthis_toolbox');
+      });
 }
 
-// Trigger the function on both jquery's ready event and turbolinks page:change event
-$(document).on('turbolinks:load', function() {
-    initAdthis();
-});
+$(document).ready(addthis);
+$(document).on('turbolinks:load', addthis);
+$(document).on('page:change', addthis);
