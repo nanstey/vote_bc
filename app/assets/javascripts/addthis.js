87c87ea0d66fc404dff106f02676ee7b33a33620
Dace Hunter <dacehunter@gmail.com>
2017-05-19 01:32:57 +0000

    adds fade in effect for addthis buttons

--- app/assets/javascripts/addthis.js
+++ app/assets/javascripts/addthis.js
@@ -1,31 +1,36 @@
 var addthis;
 addthis = function() {
-    var script = 'http://s7.addthis.com/js/300/addthis_widget.js#pubid=ra-590931e84018f1e1#async=1';
-      // Remove all global properties set by addthis, otherwise it won't reinitialize
-      for (var i in window) {
-        if (/^addthis/.test(i) || /^_at/.test(i)) {
-          delete window[i];
-        }
-      }
-      window.addthis_share = null;
+  var script = 'http://s7.addthis.com/js/300/addthis_widget.js#pubid=ra-590931e84018f1e1#async=1';
+  // Remove all global properties set by addthis, otherwise it won't reinitialize
+  for (var i in window) {
+    if (/^addthis/.test(i) || /^_at/.test(i)) {
+      delete window[i];
+    }
+  }
+  window.addthis_share = null;
 
-      // Remove all global properties set by addthis, otherwise it won't reinitialize
-      if(window.addthis) {
-           window.addthis = null;
-            window._adr = null;
-            window._atc = null;
-            window._atd = null;
-            window._ate = null;
-            window._atr = null;
-            window._atw = null;
-      }
+// Remove all global properties set by addthis, otherwise it won't reinitialize
+  if(window.addthis) {
+    window.addthis = null;
+    window._adr = null;
+    window._atc = null;
+    window._atd = null;
+    window._ate = null;
+    window._atr = null;
+    window._atw = null;
+  }
 
-      // Finally, load addthis
-      $.getScript(script, function(){
-        addthis.toolbox('.addthis_toolbox');
-      });
+  // Finally, load addthis
+  $.getScript(script, function(){
+    addthis.toolbox('.addthis_toolbox');
+  });
+
+  $(".addthis_inline_share_toolbox").bind('DOMNodeInserted', function(){
+    $(".addthis_inline_share_toolbox").fadeTo(500, 1);
+  });
 }
 
 $(document).ready(addthis);
 $(document).on('turbolinks:load', addthis);
 $(document).on('page:change', addthis);
+
