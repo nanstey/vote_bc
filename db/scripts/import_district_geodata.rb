9224e18816362d90fee48b9f0129015587b9bf1c
Couragyn <couragyn@gmail.com>
2021-01-22 05:04:08 +0100

    Cariboo-Chilcoltin working now so removed exception from boundary code

--- db/scripts/import_district_geodata.rb
+++ db/scripts/import_district_geodata.rb
@@ -5,10 +5,6 @@ districts = Election.find_by(:year => Election.current_election_year).districts
 # iterates through each district
 districts.each do |district|
   puts "  >> #{district.name}"
-  if district.name == "Cariboo-Chilcoltin"
-    next
-  end
-  # gets the centoid data from opennorth
   url = "https://represent.opennorth.ca/boundaries/british-columbia-electoral-districts-2015-redistribution/#{district.slug}/centroid"
   response = HTTP.get(url)
   centroid = JSON.parse(response)
